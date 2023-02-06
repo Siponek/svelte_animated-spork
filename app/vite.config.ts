93de6c62cde25ef74e04d3a14c204589d6727127
Siponek <szinkowicz@gmail.com>
2023-02-07 00:14:21 +0100

    Added local version for vite dev serv

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,30 +1,54 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
-const OUTER_PORT = process.env.OUTER_PORT_FRONTEND
-const INNER_PORT = process.env.INNER_PORT_FRONTEND
-// const OUTER_PORT = process.env.OUTER_PORT_FRONTEND
-// const INNER_PORT = process.env.INNER_PORT_FRONTEND
-// const OUTER_PORT = import.meta.env.VITE_OUTER_PORT_FRONTEND
-// const INNER_PORT = import.meta.env.VITE_INNER_PORT_FRONTEND
-console.log("Port for client browser_> OUTER_PORT", OUTER_PORT)
-console.log("Port exposed in Vite_> INNER_PORT", INNER_PORT)
-console.log(`You propably want to connect via http://localhost:${OUTER_PORT}/`)
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		host: "0.0.0.0",
-		hmr: {
-		  clientPort: OUTER_PORT,
+let OUTER_PORT, INNER_PORT: string;
+
+if (process.env.OUTER_PORT_FRONTEND) {
+	OUTER_PORT = process.env.OUTER_PORT_FRONTEND;
+} else {
+	OUTER_PORT = '3000';
+}
+if (process.env.INNER_PORT_FRONTEND) {
+	INNER_PORT = process.env.INNER_PORT_FRONTEND;
+} else {
+	INNER_PORT = '3000';
+}
+console.log('Port for client browser_> OUTER_PORT', OUTER_PORT);
+console.log('Port exposed in Vite_> INNER_PORT', INNER_PORT);
+console.log(`You propably want to connect via http://localhost:${OUTER_PORT}/`);
+let config: UserConfig;
+if (process.env.LOCAL_DEV) {
+	console.log(
+		'Using local dev config (no HMR, no polling, no host binding, no port binding, no port forwarding)'
+	);
+	config = {
+		plugins: [sveltekit()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		}
+		// server: {
+		// 	host: '0.0.0.0',
+		// 	port: +INNER_PORT
+		// }
+	};
+} else {
+	console.log('Using docker config');
+	config = {
+		plugins: [sveltekit()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
 		},
-		port: INNER_PORT, 
-		watch: {
-		  usePolling: true,
-		},
-	},
-};
+		server: {
+			host: '0.0.0.0',
+			hmr: {
+				clientPort: +OUTER_PORT
+			},
+			port: +INNER_PORT,
+			watch: {
+				usePolling: true
+			}
+		}
+	};
+}
 
 export default config;
