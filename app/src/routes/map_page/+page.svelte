f9409abb91cefe69d37c36959f64a04b6d0f6ec5
Siponek <szinkowicz@gmail.com>
2023-03-11 19:58:12 +0100

    Made map_page modular with components

--- app/src/routes/map_page/+page.svelte
+++ app/src/routes/map_page/+page.svelte
@@ -1,27 +1,22 @@
 <script>
-	import Selector from '$lib/Selector.svelte';
-	import LeafletMap from '$lib/LeafletMap.svelte';
-	import Map from '$lib/Map.svelte';
-	import Map_test from '$lib/Map_test.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	import Main from '$lib/Main.svelte';
+	import Sidebar from '$lib/Sidebar.svelte';
+
+	let open = false;
 </script>
 
-<div class="container mx-auto py-3">
-	<div class="text-center text-3xl font-bold">Title placeholder</div>
-	<div class="grid gap-3 md:grid-cols-4">
-		<div class="grid gap-y-3">
-			<div class="bg-gray-300">Chart 1 placeholder</div>
-			<div>Chart 2 placeholder</div>
-			<div>Check indicators placeholder</div>
-		</div>
-		<div class="bg-gray-300 md:col-span-3 md:col-start-2"><Selector /></div>
-		<div class="bg-gray-300 md:col-span-3 md:col-start-2"><Map /></div>
-		<!-- <div class="bg-gray-300 md:col-span-3 md:col-start-2"><Map_test /></div> -->
-	</div>
-	<div class="bg-gray-200">Footer placeholder</div>
-</div>
+<Sidebar bind:open />
+<Navbar bind:sidebar={open} />
+<Main />
 
 <style lang="postcss">
-	:global(html) {
-		background-color: theme('colors.gray.400');
+	/* :global(html) {
+		background-color: theme('colors.ets-grey');
+	} */
+	:global(body) {
+		background-color: theme('colors.ets-grey');
+		color: azure;
+		padding: 0;
 	}
 </style>
