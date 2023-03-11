c23b2ae7a4ad83b28ae81fe9b6453e470b6f1679
Siponek <szinkowicz@gmail.com>
2023-03-11 13:11:08 +0100

    Updated store data with italy regions data

--- app/src/lib/store.js
+++ app/src/lib/store.js
@@ -1,6 +1,11 @@
 import { disableScrollHandling } from '$app/navigation';
 import { writable, readable } from 'svelte/store';
+import geography from '$lib/data/italy_regions.json';
 // Stores are used to share data between components
+
+export let geographyData = writable(geography);
+
+// This is a writable store, which means it can be updated. Used for selecting disctricts
 export let district = writable(1);
 export const colors = readable(['#7ca6b4', '#a1d6e2', '#d5e5e5', '#6b7f8c', '#4e4e4e', '#1DB6F2']);
 
