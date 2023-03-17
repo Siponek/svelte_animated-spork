8c2105e84b17b4dd4a46617af3200fb8bf600af8
Siponek <szinkowicz@gmail.com>
2023-03-17 11:19:25 +0100

    Removed map-gl dependency

--- app/src/lib/Main.svelte
+++ app/src/lib/Main.svelte
@@ -1,8 +1,8 @@
 <script>
 	import Selector from '$lib/Selector.svelte';
 	import LeafletMap from '$lib/LeafletMap.svelte';
-	import Map from '$lib/Map.svelte';
-	import Map_test from '$lib/Map_test.svelte';
+	// import Map from '$lib/Map.svelte-decrapted';
+	// import Map_test from '$lib/Map_test.svelte';
 </script>
 
 <main class="  mx-auto  w-fill">
