d9ffc7a598e536e931724c4b738ab5568ce4f30a
Siponek <szinkowicz@gmail.com>
2023-03-29 18:46:56 +0200

    Added config for API

---
+++ app/src/apiConfig.js
@@ -0,0 +1,7 @@
+export const apiConfig = {
+	// This is the base URL of the local API server
+	// base: 'http://localhost:3000/app_ets',
+	// This is the base URL of the API server
+	base: 'http://135.181.209.141:8000/app_ets',
+	debugMessages: true
+};
--- app/src/lib/Api.js
+++ app/src/lib/Api.js
@@ -1,8 +1,9 @@
 import { error } from '@sveltejs/kit';
+import { apiConfig } from '../apiConfig';
 import { loadEnv } from 'vite';
 
-const base = 'http://127.0.0.1:8000/app_ets';
-// get that from env
+// TODO get that from env
+const base = apiConfig.base;
 
 async function send({ method, path, data, token }) {
 	const opts = { method, headers: {} };
@@ -16,32 +17,36 @@ async function send({ method, path, data, token }) {
 	if (token) {
 		opts.headers['Authorization'] = `Token ${token}`;
 	}
+	apiConfig.debugMessages && console.log('api.js', method, path, data, token);
 	const res = await fetch(`${base}/${path}`, opts);
 	if (res.ok || res.status === 422) {
 		const text = await res.text();
 		const response_obj = { headers: res.headers, body: JSON.parse(text) };
 		return text ? response_obj : {};
 	}
-	console.log('error in api.js');
+	if (apiConfig.debugMessages) {
+		console.log('error in api.js');
+		console.log(res.status);
+		console.log(res.statusText);
+		for (const [key, value] of res.headers.entries()) {
+			console.log(`${key}: ${value}`);
+		}
+	}
 	throw error(res.status, res.statusText, res.text);
 }
 
 export function get(path, token) {
-	console.log('Api.js get called');
 	return send({ method: 'GET', path, token });
 }
 
 export function del(path, token) {
-	console.log('Api.js del called');
 	return send({ method: 'DELETE', path, token });
 }
 
 export function post(path, data, token) {
-	console.log('Api.js post called');
 	return send({ method: 'POST', path, data, token });
 }
 
 export function put(path, data, token) {
-	console.log('Api.js put called');
 	return send({ method: 'PUT', path, data, token });
 }
