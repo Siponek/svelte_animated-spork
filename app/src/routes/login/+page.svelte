c6671037cd37304c14b5234a2a201d5a0bb72737
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-03 14:29:25 +0200

    update

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -2,7 +2,7 @@
 	import { enhance } from '$app/forms';
 	import ListErrors from '$lib/ListErrors.svelte';
 	// /** @type {import('./$types').ActionData} */
-	export let form: any;
+    export let form:any;
 </script>
 
 <svelte:head>
