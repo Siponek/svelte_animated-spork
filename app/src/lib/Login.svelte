90bcbac9d698396032fd39dcb6c2fe050ee78cf3
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-04 08:12:59 +0200

    styled login form

--- app/src/lib/Login.svelte
+++ app/src/lib/Login.svelte
@@ -37,7 +37,6 @@
         justify-content: center;
         align-content: center;
         text-align: center;
-        border:2px solid red;
         gap:15px;
         margin:15px;
         height:100vh;
@@ -48,9 +47,11 @@
         flex-flow:column;
         margin:35px;
         gap:35px;
+        text-align:center;
     }
     input{
         height:50px;
+        width:500px;
         border-radius:15px;
         border:1px solid green;
         color:black;
@@ -68,13 +69,26 @@
         border-radius:15px;
         border:2px solid black;
     }
+    .inputs{
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        flex-flow: column;
+        align-items: center;
+    }
+
 </style>
 <div class="loginForm">
     <h1>Non hai ancora un'account, puoi registrarti da qua</h1>
     <a href="/register">Registrati da qua</a>
     <form on:submit|preventDefault={handleLogin}>
-        <input type="email" bind:value={email} placeholder="email"/>
-        <input type="password" bind:value={password} placeholder="password"/>
+        <div class="inputs">
+            <label for="email">email</label>
+            <input type="email" bind:value={email} placeholder="email"/>
+            <label for="password">password</label>
+            <input type="password" bind:value={password} placeholder="password"/>
+        </div>
+
         <div class="btn">
             <button type="submit">Login</button>
         </div>
