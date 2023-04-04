bff00f72cdd9e164adcadda51af077311c62e63c
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-04 08:20:09 +0200

    modified static sidebar

--- app/src/lib/StaticSidebar.svelte
+++ app/src/lib/StaticSidebar.svelte
@@ -6,41 +6,20 @@
     import Menu from '$lib/Menu.svelte'
 	import { icon_layer_status, region_layer_status, user_status, user_register_status } from '$lib/store.js';
 	import UserRegisterIcon from './UserRegisterIcon.svelte';
-    //import UserPopup from '$lib/userNotifications/userPopup.svelte';
+    import UserPopup from '$lib/userNotifications/userPopup.svelte';
 
 	function toggle_store_layer(store_boolean) {
 		console.log('Sidebad button: toggling store layer');
 		store_boolean.update((n) => !n);
 	}
 
-	const btn_status = new Array(4).fill(false);
+	const btn_status = new Array(2).fill(false);
 	let userPopupStatus = false;
 </script>
 
  <aside class=" rounded-r-lg content-center relative object-none staticSidebar">
     <Menu/>
-         <LayerButton class ="p-1 icon fill-ets-blue {btn_status[0] ? 'selected' : ''}"  on:click={() => {
-            toggle_store_layer(icon_layer_status);
-            btn_status[0] = !btn_status[0];
-        }
-        }  />
-        <RegionButton class ="p-1 icon fill-ets-blue  {btn_status[1] ? 'selected' : ''}" on:click={() => {
-            toggle_store_layer(region_layer_status);
-            btn_status[1] = !btn_status[1];
-        }
-        }  />
-        <!-- <UserButton class ="p-1 icon user-icon text-green-600  {btn_status[2] ? 'selected' : ''}" on:click={() => {
-        toggle_store_layer(user_status)
-        btn_status[2] = !btn_status[2];
-    }
-    }  />
-    <UserRegisterIcon class ="p-1 icon user-register-icon text-green-600  {btn_status[3] ? 'selected' : ''}" on:click={() => {
-        toggle_store_layer(user_register_status)
-        btn_status[3] = !btn_status[3];
-    } }/> -->
 
-<!--
-<aside class=" rounded-r-lg content-center relative object-none ">
 	<LayerButton
 		class="p-1 icon fill-ets-blue {btn_status[0] ? 'selected' : ''}"
 		on:click={() => {
@@ -55,20 +34,18 @@
 			btn_status[1] = !btn_status[1];
 		}}
 	/>
-    <Menu/>
 
 <div>
 
 		<UserButton
 			class="p-1 icon user-icon  {btn_status[2] ? 'selected' : ''}"
 			on:click={() => {
-				btn_status[4] = !btn_status[4];
+				btn_status[2] = !btn_status[2];
 			}}
 			bind:popupVisible={userPopupStatus}
 		/>
 		<UserPopup bind:userPopupStatus />
 	</div>
--->
 </aside>
 
 <style>
